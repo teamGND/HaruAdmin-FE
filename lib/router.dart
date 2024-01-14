e48acce5bf0a68ea689a381ce18b57b29e6420fc
남연진 <ygeenee@naver.com>
2024-01-14 14:57:08 +0900

    first commit

---
+++ lib/router.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+// TODO - sidebar 는 shell router 사용하기
+
+final router = GoRouter(
+  routes: [],
+
+  debugLogDiagnostics: true, // 기본갑은 false. go router 관련 로그 찍힘
+);
---
+++ lib/utils/secure_storage.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class SecureStorage {
+  static final SecureStorage _instance = SecureStorage._internal();
+  late FlutterSecureStorage _storage;
+
+  factory SecureStorage() {
+    return _instance;
+  }
+
+  SecureStorage._internal() {
+    _storage = const FlutterSecureStorage();
+  }
+
+  FlutterSecureStorage get secureStorage => _storage;
+
+  // 토큰 가져오기
+  Future<String?> getAccessToken() async {
+    String? value = await _storage.read(key: 'accessToken');
+    return value;
+  }
+
+  // 토큰 설정하기
+  Future<void> setAccessToken(String token) async {
+    _storage.write(key: 'accessToken', value: token);
+  }
+
+  // 토큰 삭제하기
+  Future<void> deleteAccessToken() async {
+    _storage.delete(key: 'accessToken');
+  }
+}
